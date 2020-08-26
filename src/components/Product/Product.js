35686e9efcda2710c105f04f7188c59bc684d254
ShuvoDas1 <67542645+ShuvoDas1@users.noreply.github.com>
2020-08-26 23:43:47 +0600

    add to cart complete

---
+++ src/components/Product/Product.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faCartPlus } from '@fortawesome/free-solid-svg-icons'
+import './Product.css'
+const Product = (props) => {
+    console.log(props.product)
+    const { name, img, price, stock, seller } = props.product;
+    return (
+        <div className='product'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className='product-info'>
+                <h3 class='product-name'>{name}</h3>
+                <small>by:{seller}</small>
+                <br />
+                <p>${price}</p>
+                <p>only {stock} left in stock-order soon</p>
+                <button class='main-btn'><FontAwesomeIcon icon={faCartPlus} />add to cart</button>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
